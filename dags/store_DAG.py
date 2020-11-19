ec5563a98757568b5c8479f1dc3d64cc920a80d3
estherjsuh <estherjsuh@gmail.com>
2020-11-19 14:45:17 -0800

    add new tasks to email aggregated files; change files name to include yesterdays date

--- dags/store_DAG.py
+++ dags/store_DAG.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.mysql_operator import MySqlOperator
+from airflow.operators.email_operator import EmailOperator
 
 from datacleaner import data_cleaner
 
@@ -12,6 +13,8 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
+
+yesterday_date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
 
 dag = DAG("store_dag", default_args=default_args, schedule_interval='@daily', template_searchpath=['/usr/local/airflow/sql_files'], catchup=False)
 
@@ -24,5 +27,18 @@ t3 = MySqlOperator(task_id="create_mysql_table", mysql_conn_id="mysql_conn", sql
 
 t4 = MySqlOperator(task_id="insert_into_table", mysql_conn_id="mysql_conn", sql="insert_into_table.sql", dag=dag)
 
+t5 = MySqlOperator(task_id="select_from_table", mysql_conn_id="mysql_conn", sql="select_from_table.sql", dag=dag)
+
+t6 = BashOperator(task_id="move_file", bash_command='cat ~/store_files_airflow/location_wise_profit.csv && mv ~/store_files_airflow/location_wise_profit.csv ~/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date, dag=dag)
+
+t7 = BashOperator(task_id="move_file_2", bash_command='cat ~/store_files_airflow/store_wise_profit.csv && mv ~/store_files_airflow/store_wise_profit.csv ~/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date, dag=dag)
+
+t8 = EmailOperator(task_id="send_email", to="estherjsuh@protonmail.com", 
+    subject="Daily report generated",
+    html_content="""<h1>Yesterday's store reports are ready.</h1>""",
+    files=['usr/local/airflow/store_files_airflow/location_wise_profit_%s.csv' % yesterday_date, 'usr/local/airflow/store_files_airflow/store_wise_profit_%s.csv' % yesterday_date], dag=dag)
+
+t9 = BashOperator(task_id="rename_raw", bash_command='mv ~/store_files_airflow/raw_store_transactions.csv ~/store_files_airflow/raw_store_transactions_%s.csv' % yesterday_date, dag=dag)
+
 #use bit operators instead of set_upstream & set_downstream
-t1 >> t2 >> t3 >> t4
+t1 >> t2 >> t3 >> t4 >> t5 >> [t6,t7] >> t8 >> t9
